fc9b6a8126d9b83fe1a39902032d561838f40d47
AllyMarthaJ <ally@piecewise.org>
2025-12-18 14:03:25 +1100

    Oops

--- Formula/ribbit.rb
+++ Formula/ribbit.rb
@@ -2,7 +2,7 @@ class GitReabsorb < Formula
   desc "Vibed-up \"live\" demos"
   homepage "https://github.com/AllyMarthaJ/ribbit"
   url "https://github.com/AllyMarthaJ/ribbit/archive/refs/tags/0.1.0.tar.gz"
-  sha256 ""e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
   license "MIT"
 
   depends_on "rust" => :build
